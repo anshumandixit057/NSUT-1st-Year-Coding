fb86057b7deafe88497705abd63b8b7493ab76d3
anshumandixit057 <anshumandixit057@gmail.com>
2026-08-18 23:48:50 +0530

    for started

--- Chapter 4/01_WhileValues.c
+++ Chapter 4/01_WhileValues.c
@@ -5,7 +5,8 @@ int main(void) {
     while (i<=10)
     {
      printf("The value of i is %d!\n",i);
-     i++;
+     i++; // can also use i+=2; 
+     // so output will 1 3 5 7 9
     }   
     return 0;
 }
---
+++ Chapter 4/05_for.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    int n=7;
+    for (int i=3; i<n; i++){
+        printf(" %d\n", i);
+    }
+    return 0;
+}
